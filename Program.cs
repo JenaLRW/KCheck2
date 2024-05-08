26c48f9e1135dbad55d3ede8c940cbeb8b130027
Jena <153410246+JenaLRW@users.noreply.github.com>
2024-05-08 13:40:10 -0400

    Update Program.cs
    fixed the boolean issue

--- Program.cs
+++ Program.cs
@@ -3,42 +3,67 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
+Console.WriteLine("How many records do you want to add? ");
+    
+int numberOfRecords = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("How many records do you want to add? ");
+List<Scoring> recordList = new List<Scoring>();
 
-    var numberOfRecords = int.Parse(Console.ReadLine());
+for (int i = 0; i < numberOfRecords; i++)
+{ 
 
-    var recordList = new Tests();
+    Scoring scoring = new Scoring();
 
-    for (int i = 0; i < numberOfRecords; i++)
+    Console.WriteLine("Enter the name of horse ");
+
+    scoring.Name = Console.ReadLine();
+
+    Console.WriteLine("Enter the breed of horse ");
+
+    scoring.Breed = Console.ReadLine();
+
+    Console.WriteLine("Enter the sex of horse ");
+
+    scoring.Sex = Console.ReadLine();
+
+    Console.WriteLine("Enter the height of horse (in hands)");
+
+    scoring.Height = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the enrolling class ");
+
+    scoring.ClassTest = Console.ReadLine();
+
+    Console.WriteLine("Do you wish to qualify the score? ");
+
+    scoring.QualifyingScore = Console.ReadLine();
+
+    bool result;
+
+    if (scoring.QualifyingScore.ToLower() == "yes")
+    {
+        result = true;
+    }
+    else if (scoring.QualifyingScore.ToLower() == "no")
+    {
+        result = false;
+    }
+    else 
+    {
+        Console.WriteLine("Invalid input.  Please enter yes or no.");
+        return;
+    }
+
+    recordList.Add(scoring);
+}
+
+
+    foreach (var scoring in recordList)
     {
 
-        var tests = new Tests();
-
-        Console.WriteLine("Enter the name of horse ");
-
-        tests.Name = Console.ReadLine();
-
-        Console.WriteLine("Enter the breed of horse ");
-
-        tests.Breed = Console.ReadLine();
-
-        Console.WriteLine("Enter the sex of horse ");
-
-        tests.Sex = Console.ReadLine();
-
-        Console.WriteLine("Enter the height of horse (in hands)");
-
-        tests.Height = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Enter the enrolling class ");
-
-        tests.ClassTest = Console.ReadLine();
-
-        Console.WriteLine("Do you wish to qualify the score? ");
-
-        tests.QualifyingScore = (Console.ReadLine() == "Y");
-
-        recordList.Add(Tests);
+        Console.WriteLine($"Name: {scoring.Name}, Breed: {scoring.Breed}, Sex: {scoring.Sex}, Height: {scoring.Height}, Enrolled class: {scoring.ClassTest}, Score qualifying: {scoring.QualifyingScore}");
 
     }
+
+        
+        
