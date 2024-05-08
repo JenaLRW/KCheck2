aa144449e408f986b10700a97ffbf673c6c34c02
Jena <153410246+JenaLRW@users.noreply.github.com>
2024-05-08 13:40:38 -0400

    Add files via upload

---
+++ Scoring.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCheck2
+{
+    internal class Scoring : Demographics
+    {
+        public string ClassTest { get; set; }
+
+        public string QualifyingScore { get; set; }
+       
+        
+    }
+}
